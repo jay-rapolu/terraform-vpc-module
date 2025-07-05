437848581b31baca3e592ec4d970c93448dbb009
jay-rapolu <jay.rapolu@outlook.com>
2025-07-05 21:31:36 +0530

    subnet_id

--- data.tf
+++ data.tf
@@ -10,6 +10,18 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "public_subnets" {
+  value = aws_subnet.public.id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private.id
+}
+
+output "database_subnets" {
+  value = aws_subnet.database.id
+}
+
 output "route_table" {
   value = data.aws_vpc.default.main_route_table_id
 }
